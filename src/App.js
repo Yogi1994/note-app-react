8b11db7aa69d55e4995a9a2e3324e1ff004866d8
Yogesh Chauhan <yc.yogesh007@gmail.com>
2017-06-20 01:31:15 +0530

    removed useless commented code

--- src/App.js
+++ src/App.js
@@ -23,57 +23,10 @@ class App extends Component {
     this.props.appInit();
   }
 
-
-  // handleChangeInput(event) {
-  //   this.setState({dirToCreate: event.target.value});
-  // }
-
-  // createDirectory(){
-  //   if(this.state.dirToCreate !== ""){
-  //     this.setState({dirToCreate:""});
-  //     this.props.createDirectory(this.state.dirToCreate, this.props.filePath);
-  //   }
-  // }
-
-  // onClickBack(){
-  //   var path = this.props.filePath;
-  //   path = path.slice(0,path.length -1);
-  //   this.props.fetchDirList(path);
-  // }
-
-  // getPath() {
-  //   var path = "";
-  //   for( var i =0 ; i< this.props.filePath.length; i++){
-  //     path += this.props.filePath[i] + " > ";
-  //   }
-  //   return path;
-  // }
-
   render() {
-    // if(!this.props.dirList) {
-    //   return (
-    //     <p>Loading ... </p>
-    //   );
-    // }
+    
     return (
       <div className="App">
-       {/*
-               <div className="container topContainer">
-                 
-                 <div className="row">
-                   <div className="col-md-3">
-                     <button type="button" className="btn" onClick={this.onClickBack.bind(this)}>Back</button>
-                   </div>
-                   <div className="col-md-6">
-                     <input type="text" className="form-control" value={this.state.dirToCreate} onChange={this.handleChangeInput.bind(this)} />
-                   </div>
-                   <div className="col-md-3">
-       
-                     <button type="button" className="btn" onClick={this.createDirectory.bind(this)}>Create</button>
-                   </div>
-                 </div>
-               </div>
-       */}
         <div className="container">
           <div className="fileContainer row">
             <AllNotes />
@@ -86,8 +39,7 @@ class App extends Component {
 
 function mapStateToProps(state : Object) {
   return {
-    // dirList: state.appReducer.dirList,
-    // filePath: state.appReducer.filePath
+    
   };
 }
 
@@ -95,10 +47,7 @@ function mapDispatchToProps(dispatch : Function) {
   return {
     appInit: () => {
       dispatch(AppActions.appInit());
-    },
-    // createDirectory: (dir:string, filePath: Array<string>) => {
-    //   dispatch(AppActions.createDirectory(dir, filePath));
-    // }
+    }
   };
 }
 
